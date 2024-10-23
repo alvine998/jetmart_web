2298a193466bd658f8873b196d75effe8ea02bee
alvin <alvinecom2018@gmail.com>
2024-10-23 17:01:16 +0700

    mobile responsive

--- src/components/navbar/NavHome.tsx
+++ src/components/navbar/NavHome.tsx
@@ -1,10 +1,17 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
-import { PackageOpenIcon, ShoppingBag } from "lucide-react";
+import {
+  EclipseIcon,
+  EllipsisVerticalIcon,
+  PackageOpenIcon,
+  ShoppingBag,
+  XCircleIcon,
+} from "lucide-react";
 
 export default function NavHome() {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [openSide, setOpenSide] = useState<boolean>(false);
   const dropdownRef: any = useRef(null); // Reference to the dropdown element
   // Close the dropdown if clicked outside
   useEffect(() => {
@@ -65,12 +72,16 @@ export default function NavHome() {
   ];
   return (
     <div
-      className="bg-black w-full h-16 shadow px-10 py-2 flex"
+      className="bg-black w-full md:h-16 h-20 shadow md:px-10 px-2 md:py-2 py-4 flex md:gap-0 gap-2"
       ref={dropdownRef}
     >
-      <img alt="logo" src="/images/jetmart_logo.jpeg" className="h-12" />
+      <img
+        alt="logo"
+        src="/images/jetmart_logo.jpeg"
+        className="md:h-12 h-7 md:mt-0 mt-3"
+      />
 
-      <div className="flex gap-5 ml-[250px] mt-2">
+      <div className="gap-5 ml-[250px] mt-2 md:flex hidden">
         {tabs?.map((v: any, i: number) => (
           <div key={i}>
             <a
@@ -88,16 +99,16 @@ export default function NavHome() {
       </div>
 
       {/* Search */}
-      <div className="ml-[150px] mt-1">
+      <div className="md:ml-[150px] ml-0 md:mt-1 mt-2">
         <input
           type="text"
           placeholder="Cari disini"
-          className="p-2 rounded-full shadow transition-all duration-200 pl-4 focus:outline-none focus:ring focus:ring-red-700"
+          className="md:p-2 p-1 rounded-full shadow transition-all duration-200 pl-4 focus:outline-none focus:ring focus:ring-red-700"
         />
       </div>
 
       {/* Cart */}
-      <div className="ml-10 flex gap-3 items-center">
+      <div className="md:ml-10 ml-0 flex md:gap-3 gap-0 items-center">
         <button
           className="text-white"
           type="button"
@@ -105,9 +116,56 @@ export default function NavHome() {
             setIsOpen(!isOpen);
           }}
         >
-          <ShoppingBag className="w-7" />
+          <ShoppingBag className="w-10" />
         </button>
-        <p className="text-white">0</p>
+        <div className="w-5 h-5 rounded-full bg-red-600 flex justify-center items-center md:ml-0 -ml-5 md:mt-0 -mt-4">
+          <p className="text-white text-xs">0</p>
+        </div>
+      </div>
+
+      <div className="flex md:gap-3 gap-0 items-center md:hidden">
+        <button
+          className="text-white"
+          type="button"
+          onClick={() => {
+            setOpenSide(!openSide);
+          }}
+        >
+          <EllipsisVerticalIcon className="w-5" />
+        </button>
+      </div>
+      <div
+        className={`absolute inset-0 w-0 bg-white shadow transition-all duration-200 h-full ease-in-out ${
+          openSide ? "w-[20rem] p-2" : ""
+        }`}
+      >
+        <div className={openSide ? "block" : "hidden"}>
+          <div className="flex flex-col gap-5 mt-2">
+            <div className="flex justify-end">
+              <button
+                onClick={() => {
+                  setOpenSide(!openSide);
+                }}
+              >
+                <XCircleIcon />
+              </button>
+            </div>
+            {tabs?.map((v: any, i: number) => (
+              <div key={i}>
+                <a
+                  href={v?.href}
+                  className={`text-xl text-center font-sans font-semibold transition-all duration-200 ${
+                    v?.current == router.pathname
+                      ? "text-red-700"
+                      : "text-black hover:text-red-700"
+                  }`}
+                >
+                  {v?.name}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       {/* Dropdown panel */}
       {isOpen && (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavHome from "@/components/navbar/NavHome";
 import BottomNav from "@/components/navbar/BottomNav";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Jetmart</title>
+      </Head>
       <NavHome />
 
-      {/* Banner */}
       <div className="w-full h-[600px] bg-white flex items-center justify-center">
         <img
           alt="product"
@@ -19,7 +22,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="px-20 py-10">
+      <div className="px-4 sm:px-10 md:px-20 md:py-10 py-2">
         <img
           alt="note"
           src="https://firebasestorage.googleapis.com/v0/b/jetmart-25181.appspot.com/o/keterangan.png?alt=media&token=c2830f42-48c7-412d-8c26-971b7472a69a"
@@ -27,34 +30,36 @@ export default function Home() {
         />
       </div>
 
-      <div className="bg-white w-full h-auto p-2 flex justify-between items-center px-10 mt-10 relative z-0 gap-5">
-        <div className="shadow-lg bg-white rounded w-1/2 h-[300px]">
-          <h1 className="font-sans text-2xl font-bold text-gray-700 mt-2 ml-10">
+      <div className="bg-white w-full h-auto p-2 flex flex-col md:flex-row justify-between items-center md:px-10 mt-10 relative z-0 gap-5">
+        {/* First Block */}
+        <div className="shadow-lg bg-white rounded w-full md:w-1/2 h-[300px]">
+          <h1 className="font-sans text-xl sm:text-2xl font-bold text-gray-700 mt-2 ml-5 md:ml-10">
             DTF Machine
           </h1>
-          <div className="flex justify-between items-center pr-5">
+          <div className="flex flex-col md:flex-row justify-between items-center pr-5">
             <img
               alt="product"
               src="/images/machine.png"
               className="relative z-0 -mt-10"
             />
-            <button className="bg-white rounded-full border-2 py-1 px-2 w-[250px] relative z-10 mt-10 transition-all duration-200 hover:bg-red-700 hover:text-white">
+            <button className="bg-white rounded-full border-2 py-1 px-2 w-[200px] sm:w-[250px] relative z-10 mt-14 md:mt-10 transition-all duration-200 hover:bg-red-700 hover:text-white">
               Selengkapnya
             </button>
           </div>
         </div>
 
-        <div className="shadow-xl bg-white rounded w-1/2 h-[300px]">
-          <h1 className="font-sans text-2xl font-bold text-gray-700 mt-2 ml-10">
+        {/* Second Block */}
+        <div className="shadow-xl bg-white rounded w-full md:w-1/2 h-[300px] md:mt-0 mt-10">
+          <h1 className="font-sans text-xl sm:text-2xl font-bold text-gray-700 mt-2 ml-5 md:ml-10">
             Bahan Baku DTF
           </h1>
-          <div className="flex justify-between items-center pr-20">
+          <div className="flex flex-col md:flex-row justify-between items-center md:pr-20">
             <img
               alt="product"
               src="https://firebasestorage.googleapis.com/v0/b/jetmart-25181.appspot.com/o/consum1.png?alt=media&token=ac72e22e-28c2-41a5-9a2d-b5ea94acf68a"
-              className="relative z-0 -mt-40 w-full h-[500px]"
+              className="relative z-0 -mt-10 md:-mt-40 w-full md:w-auto h-[300px] md:h-[500px]"
             />
-            <button className="bg-white rounded-full border-2 py-1 px-2 w-[250px] relative z-10 mt-24 transition-all duration-200 hover:bg-red-700 hover:text-white">
+            <button className="bg-white rounded-full border-2 py-1 px-2 w-[200px] sm:w-[250px] relative z-10 mt-5 md:mt-24 transition-all duration-200 hover:bg-red-700 hover:text-white">
               Selengkapnya
             </button>
           </div>
